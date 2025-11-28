db1df726d8e0259279c580dc0df878bd0586e5d8
Jeremy Collins <jeremy@jscollins.co>
2025-11-28 00:00:44 -0600

    Add moving asteroids and asteroid field spawning
    - Implement Asteroid class inheriting from CircleShape
    - Draw asteroids as white circles using pygame.draw.circle
    - Update asteroid positions using velocity and delta time
    - Set up asteroid, updatable, and drawable sprite groups in main
    - Wire Asteroid and AsteroidField into the main game loop
    - Register AsteroidField as an updatable spawner of new asteroids

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
@@ -19,12 +21,15 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
 
     # Create the player in the center of the screen
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
-
+    asteroid_field = AsteroidField()
 
     # Main game loop
     while True:
@@ -42,7 +47,7 @@ def main():
         # Update all updatable objects
         updatable.update(dt)
 
-        #Draw all drawable objects
+        # Draw all drawable objects
         for sprite in drawable:
             sprite.draw(screen)
 
